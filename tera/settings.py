e4d308bfdcbb6c75a957c24544f3cbb90d06f282
JoseDevApps <Lionhard_2.6@hotmail.com>
2025-03-08 13:24:42 -0400

    first commit

--- tera/settings.py
+++ tera/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gp951rdm)y#jxrb=bfrmho8ej#)5_p_$-(pln25w9j%r50mb&)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
